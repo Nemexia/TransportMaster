b7abf5a53a3b8472d29e187b1f74ed123f631db8
Nemexia <nemexia.m@gmail.com>
2026-08-13 14:37:21 +0330

    Change main to minimum window draw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-
+#include <raylib.h>
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    InitWindow(800, 600, "TransportMaster");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
